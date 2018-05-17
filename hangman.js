2735e4eadf76724ae03aa21668186eeff21b9177
Sampsa Saari <sampsa.saari@gmail.com>
2018-05-17 18:29:28 +0300

    Basic functionality.

---
+++ hangman.js
@@ -0,0 +1,24 @@
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('');
+    this.remainingGuesses = remainingGuesses;
+    this.guessedLetters = ['c'];
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = '';
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter;
+        } else {
+            puzzle += '*';
+        }
+    });
+    return puzzle;
+}
+
+const game1 = new Hangman('Cat', 2);
+console.log(game1.getPuzzle());
+
+const game2 = new Hangman('New Jersey', 3);
+console.log(game2.getPuzzle());
